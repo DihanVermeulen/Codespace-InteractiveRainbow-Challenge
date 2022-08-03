1fbe841068b55f89b9918e96babfa7733be1a01e
Dihan_Vermeulen <74895608+DihanVermeulen@users.noreply.github.com>
2022-08-03 10:34:44 +0200

    Updated script

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 function changeColour(element, colour) {
     element.style.backgroundColor = colour;
     element.innerHTML = colour;
-
 }
-
-document.getElementById("red").click = style.backgroundColor = red;
